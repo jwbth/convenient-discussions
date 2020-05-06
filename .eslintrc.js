e619b191782136c973f2ff97be9e286a4bf1e0bc
jwbth <33615628+jwbth@users.noreply.github.com>
2020-05-06 14:11:11 +0300

    Return convenientDiscussions to list of globals in .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     "Parser": "readonly",
     "Section": "readonly",
     "User": "readonly",
+    "convenientDiscussions": "readonly",
     "mw": "readonly",
     "require": "readonly",
   },
