15e175b196f9d57651ceb53f76aa6fa6c1effe92
jwbth <33615628+jwbth@users.noreply.github.com>
2020-07-30 10:39:53 +0300

    Rename "-test" to "-dev" builds (2)
    Previous commit: 1ca1922023c0984eb1a5f3b54e9bde0cefaa8803

--- buildConfigs.js
+++ buildConfigs.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const argv = require('yargs').argv;
 
-// node buildConfigs --test
-// npm run <command running this script> --test
-const testSuffix = (argv.test || process.env.npm_config_test) ? '-test' : '';
+// node buildConfigs --dev
+// npm run <command running this script> --dev
+const devSuffix = (argv.dev || process.env.npm_config_dev) ? '-dev' : '';
 
 const configs = [];
 fs.readdirSync('./config/').forEach((file) => {
@@ -75,7 +75,7 @@ function getStrings() {
 
 if (!cdLoaded) {
   convenientDiscussions.getStringsPromise = getStrings();
-  mw.loader.getScript('https://commons.wikimedia.org/w/index.php?title=User:Jack_who_built_the_house/convenientDiscussions${testSuffix}.js&action=raw&ctype=text/javascript')
+  mw.loader.getScript('https://commons.wikimedia.org/w/index.php?title=User:Jack_who_built_the_house/convenientDiscussions${devSuffix}.js&action=raw&ctype=text/javascript')
     .catch((e) => {
       console.warn('Couldn\\'t load Convenient Discussions.', e);
     });
@@ -84,7 +84,7 @@ if (!cdLoaded) {
 }());
 `;
   fs.mkdirSync('dist/config', { recursive: true });
-  fs.writeFileSync(`dist/config/${config.name}${testSuffix}.js`, data);
+  fs.writeFileSync(`dist/config/${config.name}${devSuffix}.js`, data);
 });
 
 console.log('Configs have been built successfully.');
