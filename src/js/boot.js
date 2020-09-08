jwbth/convenient-discussions

commit 8f90f12beac93232e3266a5ff8298dbdbab68607

Fix boot.initSettings

src/js/boot.js
@@ -116,12 +116,12 @@ export async function initSettings() {
   if (cd.settings.signaturePrefix) {
     // eslint-disable-next-line no-useless-escape
     cd.settings.signaturePrefix = cd.settings.signaturePrefix.replace(/~~\~~/, '')
-  }
 
-  // FIXME: Temporary, remove after some time. Transition to including spaces into signaturePrefix.
-  if (!cd.settings.signaturePrefix.startsWith(' ')) {
-    // eslint-disable-next-line no-useless-escape
-    cd.settings.signaturePrefix = ' ';
+    // FIXME: Temporary, remove after some time. Transition to including spaces into signaturePrefix.
+    if (!cd.settings.signaturePrefix.startsWith(' ')) {
+      // eslint-disable-next-line no-useless-escape
+      cd.settings.signaturePrefix = ' ';
+    }
   }
 
   if (

commit c827688fdc740752bcc63d83e193a3932b6e4b34

Fix empty signature prefix of users who used space before signature
It broke together with 8f90f12beac93232e3266a5ff8298dbdbab68607.

src/js/boot.js
@@ -113,7 +113,7 @@ export async function initSettings() {
   cd.settings = Object.assign(cd.settings, remoteSettings);
 
   // Seamless transition from mySignature.
-  if (cd.settings.signaturePrefix) {
+  if (cd.settings.signaturePrefix !== undefined) {
     // eslint-disable-next-line no-useless-escape
     cd.settings.signaturePrefix = cd.settings.signaturePrefix.replace(/~~\~~/, '')
 