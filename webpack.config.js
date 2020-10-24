fa2586bef23327c4d1d2bff44bd49bc136ee0daa
jwbth <33615628+jwbth@users.noreply.github.com>
2020-10-24 20:39:35 +0300

    Fix webpack config
    "npm run snippet" didn't work.

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const webpack = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
 const WebpackBuildNotifierPlugin = require('webpack-build-notifier');
 const BannerWebpackPlugin = require('banner-webpack-plugin');
+const argv = require('yargs').argv;
 require('json5/lib/register.js');
 
 const config = require('./config.json5');
@@ -12,8 +13,8 @@ const getUrl = require('./misc/util.js').getUrl;
 
 const lang = process.env.npm_config_lang || 'ru';
 const project = process.env.npm_config_project || 'w';
-const snippet = Boolean(process.env.npm_config_snippet);
-const dev = Boolean(process.env.npm_config_dev);
+const snippet = Boolean(argv.snippet || process.env.npm_config_snippet);
+const dev = Boolean(argv.dev || process.env.npm_config_dev);
 
 const interlanguageWikis = ['w', 'b', 'n', 'q', 's', 'v', 'voy', 'wikt'];
 const fullCode = interlanguageWikis.includes(project) ? `${project}-${lang}` : project;
