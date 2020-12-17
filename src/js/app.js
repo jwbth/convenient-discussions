ad7dfc747088940a026ff29e415a87d5d45ea88d
jwbth <33615628+jwbth@users.noreply.github.com>
2020-12-17 12:43:31 +0300

    Don't make userinfo request in advance on talk pages
    To avoid overriding the visits setting. setTimeout() turned out to not be delayed until the tab is opened.

--- src/js/app.js
+++ src/js/app.js
@@ -285,19 +285,13 @@ function go() {
       cd.debug.startTimer('loading data');
 
       // Make some requests in advance if the API module is ready in order not to make 2 requests
-      // sequentially. We use a timeout before making the userinfo request, because without it, if
-      // there is more than one tab in the background, this request is made and the execution stops
-      // at mw.loader.using, which results in overriding the renewed visits setting of one tab by
-      // another tab (the visits are loaded by one tab, then another tab, then written by one tab,
-      // then by another tab).
+      // sequentially. We don't make a userinfo request, because if there is more than one tab in
+      // the background, this request is made and the execution stops at mw.loader.using, which
+      // results in overriding the renewed visits setting of one tab by another tab (the visits are
+      // loaded by one tab, then another tab, then written by one tab, then by another tab).
       let dataRequest;
       if (mw.loader.getState('mediawiki.api') === 'ready') {
         dataRequest = loadData();
-        setTimeout(() => {
-          getUserInfo().catch((e) => {
-            console.warn(e);
-          });
-        });
       }
 
       let modulesRequest = mw.loader.using([
