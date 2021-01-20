04a85b0f3928a9df78d23a3cc747c6704275c5ad
jwbth <33615628+jwbth@users.noreply.github.com>
2021-01-20 04:57:36 +0300

    Use util.getExtendedRect to get heading margins in currentSection module
    Experiments show it's 2–3 times faster than creating dummy elements.

--- src/js/currentSection.js
+++ src/js/currentSection.js
@@ -5,9 +5,9 @@
  */
 
 import cd from './cd';
+import { getExtendedRect } from './util';
 
 let currentSection;
-const headingMargins = [];
 
 export default {
   mount() {
@@ -16,35 +16,14 @@ export default {
       .appendTo(document.body);
   },
 
-  fillHeadingMargins() {
-    cd.debug.startTimer('headingMargins');
-    const $headings = [];
-    for (let level = 1; level <= 6; level++) {
-      $headings[level] = $(`<h${level}>`);
-      cd.g.$root.append($headings[level]);
-      headingMargins[level] = parseFloat($headings[level].css('margin-top'));
-    }
-    $headings.forEach(($heading) => {
-      $heading.remove();
-    });
-    cd.debug.stopTimer('headingMargins');
-  },
-
   update() {
-    if (!headingMargins.length && cd.sections[0]) {
-      this.fillHeadingMargins();
-    }
-
     if (
       currentSection &&
       (
         document.body.scrollHeight === document.body.clientHeight ||
         window.pageYOffset === 0 ||
         !cd.sections.length ||
-        (
-          cd.sections[0].$heading.get(0).getBoundingClientRect().top >=
-          headingMargins[cd.sections[0].level]
-        )
+        getExtendedRect(cd.sections[0].$heading.get(0)).outerTop >= 0
       )
     ) {
       this.reset();
@@ -56,13 +35,13 @@ export default {
       .slice()
       .reverse()
       .some((section) => {
-        const rect = section.$heading.get(0).getBoundingClientRect();
+        const extendedRect = getExtendedRect(section.$heading.get(0));
 
         // If the element has 0 as the left position and height, it's probably invisible for some
         // reason.
-        if (rect.left === 0 && rect.height === 0) return;
+        if (extendedRect.left === 0 && extendedRect.height === 0) return;
 
-        if (rect.top < headingMargins[section.level]) {
+        if (extendedRect.outerTop < 0) {
           if (currentSection === section) {
             return true;
           }
@@ -91,4 +70,4 @@ export default {
     currentSection = null;
     this.$element.empty();
   },
-}
+};
