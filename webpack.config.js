4f8ccecc84985d6722bd77212b17ae5496af0af2
jwbth <33615628+jwbth@users.noreply.github.com>
2021-02-15 19:19:41 +0300

    Update npm modules

--- webpack.config.js
+++ webpack.config.js
@@ -113,6 +113,8 @@ module.exports = {
           options: {
             presets: ['@babel/preset-env'],
             plugins: [
+              '@babel/plugin-proposal-nullish-coalescing-operator',
+              '@babel/plugin-proposal-optional-chaining',
               '@babel/plugin-transform-runtime',
               '@babel/plugin-transform-async-to-generator',
             ],
