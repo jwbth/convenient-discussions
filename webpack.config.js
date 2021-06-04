dc4fbd7a0e200d1e5a851455e84826e004a038fb
jwbth <33615628+jwbth@users.noreply.github.com>
2021-06-04 05:18:46 +0300

    Add tests for Comment#reformatTimestamp
    Add jest npm library. Move babel configuration to .babelrc to use with jest.

--- webpack.config.js
+++ webpack.config.js
@@ -135,15 +135,6 @@ module.exports = (env) => {
           exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-              plugins: [
-                '@babel/plugin-proposal-nullish-coalescing-operator',
-                '@babel/plugin-proposal-optional-chaining',
-                '@babel/plugin-transform-runtime',
-                '@babel/plugin-transform-async-to-generator',
-              ],
-            },
           },
         },
         {
