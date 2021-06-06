jwbth/convenient-discussions

commit 069e716e269feb7c380549c9540691cbda633dee

Fix detecting interwikis in wikilinks autocomplete

src/js/boot.js
@@ -306,7 +306,7 @@ function initPatterns() {
 
   const allNamespaces = Object.keys(namespaceIds).filter((ns) => ns);
   const allNamespacesPattern = allNamespaces.join('|');
-  cd.g.ALL_NAMESPACES_REGEXP = new RegExp(`(?:^|:)(?:${allNamespacesPattern}):`, 'i');
+  cd.g.ALL_NAMESPACES_REGEXP = new RegExp(`^(?:${allNamespacesPattern}):`, 'i');
 
   const contribsPagePattern = anySpace(cd.g.CONTRIBS_PAGE);
   cd.g.CAPTURE_USER_NAME_PATTERN = (

commit 68eaa0a321a42ff20f8d872a8b75ef2a9402e3e5

Fix autocomplete for categories and files when link starts with ":"
It was broken after 069e716e269feb7c380549c9540691cbda633dee.

src/js/boot.js
@@ -390,7 +390,7 @@ function initPatterns() {
 
   const allNamespaces = Object.keys(namespaceIds).filter((ns) => ns);
   const allNamespacesPattern = allNamespaces.join('|');
-  cd.g.ALL_NAMESPACES_REGEXP = new RegExp(`^(?:${allNamespacesPattern}):`, 'i');
+  cd.g.ALL_NAMESPACES_REGEXP = new RegExp(`^:?(?:${allNamespacesPattern}):`, 'i');
 
   const contribsPagePattern = anySpace(cd.g.CONTRIBS_PAGE);
   cd.g.CAPTURE_USER_NAME_PATTERN = (