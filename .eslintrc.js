230d9179af0ceb7bebf6667f979740f7e7d16871
jwbth <33615628+jwbth@users.noreply.github.com>
2021-07-02 22:52:08 +0300

    Fix ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,7 @@ module.exports = {
   "ignorePatterns": ["dist/**", "misc/**", "*.json5", "w-he.js"],
   "overrides": [
     {
-      "files": ["./*.js", "/tribute/**"],
+      "files": ["./*.js", "src/tribute/**"],
       "rules": {
         "jsdoc/require-jsdoc": "off",
       },
