jwbth/convenient-discussions

commit 9d6395feabf695f0bf26f5dd38a8d0f3eac6079d

Fall back to "Allusers" API for username searching
The searched user may have no "User talk" page in the current wiki.

Change-Id: I93f4ed38f21517c07c140f26a3dd3338dbbdebcf

src/js/apiWrappers.js
@@ -12,7 +12,7 @@ import CdError from './CdError';
 import cd from './cd';
 import userRegistry from './userRegistry';
 import { createApi } from './boot';
-import { defined, handleApiReject, unique } from './util';
+import { defined, firstCharToUpperCase, handleApiReject, unique } from './util';
 import { unpackVisits, unpackWatchedSections } from './options';
 
 let cachedUserInfoRequest;
@@ -391,40 +391,57 @@ export async function getUserGenders(users, requestInBackground = false) {
  * @throws {CdError}
  */
 export function getRelevantUserNames(text) {
+  text = firstCharToUpperCase(text);
   const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
+    setTimeout(async () => {
       try {
         if (promise !== currentAutocompletePromise) {
           throw new CdError();
         }
 
-        cd.g.mwApi.get({
+        // First, try to use the search to get only users that have talk pages. Most legitimate
+        // users do, while spammers don't.
+        const resp = cd.g.mwApi.get({
           action: 'opensearch',
           search: text,
           namespace: 3,
           redirects: 'resolve',
           limit: 10,
           formatversion: 2,
-        }).then(
-          (resp) => {
-            const users = resp[1]
-              ?.map((name) => (name.match(cd.g.USER_NAMESPACES_REGEXP) || [])[1])
-              .filter(defined)
-              .filter((name) => !name.includes('/'));
+        }).catch(handleApiReject);
 
-            if (!users) {
-              throw new CdError({
-                type: 'api',
-                code: 'noData',
-              });
-            }
+        const users = resp[1]
+          ?.map((name) => (name.match(cd.g.USER_NAMESPACES_REGEXP) || [])[1])
+          .filter(defined)
+          .filter((name) => !name.includes('/'));
+        if (!users) {
+          throw new CdError({
+            type: 'api',
+            code: 'noData',
+          });
+        }
 
-            resolve(users);
-          },
-          (e) => {
-            handleApiReject(e);
+        if (users.length) {
+          resolve(users);
+        } else {
+          // If we didn't succeed with search, try the entire users database.
+          const resp = cd.g.mwApi.get({
+            action: 'query',
+            list: 'allusers',
+            auprefix: text,
+            formatversion: 2,
+          }).catch(handleApiReject);
+
+          const users = resp?.query?.allusers?.map((user) => user.name);
+          if (!users) {
+            throw new CdError({
+              type: 'api',
+              code: 'noData',
+            });
           }
-        );
+
+          resolve(users);
+        }
       } catch (e) {
         reject(e);
       }

commit 6b21022ba869ee760f3da058f0c642e9f3260dc6

Fix mentions autocomplete not suggesting names from search
The bug was introduced in 9d6395feabf695f0bf26f5dd38a8d0f3eac6079d.

Change-Id: I7c0770c15ab933e850649e51a0b1a88cbaa5bc3a

src/js/apiWrappers.js
@@ -399,7 +399,7 @@ export function getRelevantUserNames(text) {
 
         // First, try to use the search to get only users that have talk pages. Most legitimate
         // users do, while spammers don't.
-        const resp = cd.g.mwApi.get({
+        const resp = await cd.g.mwApi.get({
           action: 'opensearch',
           search: text,
           namespace: 3,
@@ -422,7 +422,7 @@ export function getRelevantUserNames(text) {
           resolve(users);
         } else {
           // If we didn't succeed with search, try the entire users database.
-          const resp = cd.g.mwApi.get({
+          const resp = await cd.g.mwApi.get({
             action: 'query',
             list: 'allusers',
             auprefix: text,