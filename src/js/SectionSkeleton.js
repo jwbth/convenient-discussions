jwbth/convenient-discussions

commit 9b2b1bf0625e60c47393b6feb802b37ddd44a3bc

Fix getting section elements
Use the same mechanism that we use to get thread contents, no matter the nesting.

We can't just take all the following elements until the next heading - parts of sections that include the heading can be inside elements like here: https://he.wikipedia.org/wiki/Project:חדשות#Desktop_improvements._A_new_change!.

Change-Id: I1af42df98d72a3ea4a791850eecdcf1eef26ae40

src/js/SectionSkeleton.js
@@ -1,6 +1,7 @@
 import CdError from './CdError';
 import cd from './cd';
 import { TreeWalker } from './treeWalker';
+import { getElementRangeContents } from './util';
 
 /**
  * Class containing the main properties of a section. This class is the only one used in the worker
@@ -12,8 +13,9 @@ class SectionSkeleton {
    *
    * @param {Parser} parser
    * @param {Element|external:Element} headingElement
+   * @param {Element|external:Element} nextRelevantHeadingElement
    */
-  constructor(parser, headingElement) {
+  constructor(parser, headingElement, nextRelevantHeadingElement) {
     this.parser = parser;
 
     /**
@@ -94,7 +96,7 @@ class SectionSkeleton {
       }
     }
 
-    this.setContentProperties();
+    this.setContentProperties(nextRelevantHeadingElement);
 
     /**
      * Section ID. Same as the section index in
@@ -127,8 +129,10 @@ class SectionSkeleton {
 
   /**
    * Set some properties related to the content of the section (contained elements and comments).
+   *
+   * @param {Element|external:Element} nextRelevantHeadingElement
    */
-  setContentProperties() {
+  setContentProperties(nextRelevantHeadingElement) {
     const treeWalker = new TreeWalker(
       cd.g.rootElement,
       (node) => (
@@ -148,28 +152,34 @@ class SectionSkeleton {
       this.headingNestingLevel++;
     }
 
-    treeWalker.currentNode = this.headingElement;
-    const elements = [this.headingElement];
-    const levelRegexp = new RegExp(`^H[1-${this.level}]$`);
-
-    // The last element before the next heading if we start from the heading of this section. That
-    // heading which may be a part of the next section of the same level, or the subsection of this
-    // section.
-    let hasSubsections = false;
-    while (treeWalker.nextSibling() && !levelRegexp.test(treeWalker.currentNode.tagName)) {
-      if (
-        this.lastElementInFirstChunk === undefined &&
-        /^H[2-6]$/.test(treeWalker.currentNode.tagName)
-      ) {
-        hasSubsections = true;
-        this.lastElementInFirstChunk = elements[elements.length - 1];
+    // We can't just take all the following elements until the next heading - parts of sections that
+    // include the heading can be inside elements like here:
+    // https://he.wikipedia.org/wiki/Project:חדשות#Desktop_improvements._A_new_change!
+    let elements;
+    if (nextRelevantHeadingElement) {
+      elements = getElementRangeContents(this.headingElement, nextRelevantHeadingElement);
+      elements.pop();
+    } else {
+      let lastElement = cd.g.rootElement.lastElementChild;
+      while (lastElement.contains(this.headingElement) && lastElement !== this.headingElement) {
+        lastElement = lastElement.lastElementChild;
       }
-      elements.push(treeWalker.currentNode);
+      elements = getElementRangeContents(this.headingElement, lastElement);
     }
+    let hasSubsections = false;
+    elements.slice(1).some((el) => {
+      if (this.lastElementInFirstChunk === undefined && /^H[2-6]$/.test(el.tagName)) {
+        hasSubsections = true;
+        this.lastElementInFirstChunk = el;
+        return true;
+      } else {
+        return false;
+      }
+    });
 
     /**
      * Last element in the first chunk of the section, i.e. all elements up to the first subheading
-     * if it is present, or all elements if it is not.
+     * if it is present or just all elements if it is not.
      *
      * @type {Element|external:Element}
      */

src/js/Parser.js
@@ -111,10 +111,16 @@ class Parser {
    *
    * @param {Element|external:Element} headingElement
    * @param {Promise} watchedSectionsRequest
+   * @param {Element|external:Element} nextRelevantHeadingElement
    * @returns {*}
    */
-  createSection(headingElement, watchedSectionsRequest) {
-    return new this.context.SectionClass(this, headingElement, watchedSectionsRequest);
+  createSection(headingElement, watchedSectionsRequest, nextRelevantHeadingElement) {
+    return new this.context.SectionClass(
+      this,
+      headingElement,
+      watchedSectionsRequest,
+      nextRelevantHeadingElement
+    );
   }
 
   /**

src/js/Section.js
@@ -39,10 +39,11 @@ class Section extends SectionSkeleton {
    * @param {Parser} parser A relevant instance of Parser.
    * @param {Element} headingElement
    * @param {Promise} watchedSectionsRequest
+   * @param {Element} nextRelevantHeadingElement
    * @throws {CdError}
    */
-  constructor(parser, headingElement, watchedSectionsRequest) {
-    super(parser, headingElement);
+  constructor(parser, headingElement, watchedSectionsRequest, nextRelevantHeadingElement) {
+    super(parser, headingElement, nextRelevantHeadingElement);
 
     elementPrototypes = cd.g.SECTION_ELEMENT_PROTOTYPES;
 

src/js/Thread.js
@@ -7,6 +7,7 @@ import {
   defined,
   flat,
   getCommonGender,
+  getElementRangeContents,
   getExtendedRect,
   getFromLocalStorage,
   getVisibilityByRects,
@@ -404,68 +405,6 @@ class Thread {
   }
 
   /**
-   * Get contents of the thread.
-   *
-   * @returns {Node[]}
-   * @private
-   */
-  getRangeContents() {
-    const range = document.createRange();
-    range.setStart(this.startElement, 0);
-    const rangeEnd = this.getAdjustedEndElement();
-    range.setEnd(rangeEnd, rangeEnd.childNodes.length);
-
-    /*
-      Here we should equally account for all cases of the start and end item relative position.
-
-        <ul>         <!-- Say, may start anywhere from here... -->
-          <li></li>
-          <li>
-            <div></div>
-          </li>
-          <li></li>
-        </ul>
-        <div></div>  <!-- ...to here. And, may end anywhere from here... -->
-        <ul>
-          <li></li>
-          <li>
-            <div></div>
-          </li>
-          <li></li>  <-- ...to here. -->
-        </ul>
-     */
-    const rangeContents = [range.startContainer];
-
-    // The start container could contain the end container and be different from it in the case with
-    // adjusted end items.
-    if (!range.startContainer.contains(range.endContainer)) {
-      treeWalker.currentNode = range.startContainer;
-
-      while (treeWalker.currentNode.parentNode !== range.commonAncestorContainer) {
-        while (treeWalker.nextSibling()) {
-          rangeContents.push(treeWalker.currentNode);
-        }
-        treeWalker.parentNode();
-      }
-      treeWalker.nextSibling();
-      while (!treeWalker.currentNode.contains(range.endContainer)) {
-        rangeContents.push(treeWalker.currentNode);
-        treeWalker.nextSibling();
-      }
-      while (treeWalker.currentNode !== range.endContainer) {
-        treeWalker.firstChild();
-        while (!treeWalker.currentNode.contains(range.endContainer)) {
-          rangeContents.push(treeWalker.currentNode);
-          treeWalker.nextSibling();
-        }
-      }
-      rangeContents.push(range.endContainer);
-    }
-
-    return rangeContents;
-  }
-
-  /**
    * Get a list of users in the thread.
    *
    * @returns {module:userRegistry~User[]}
@@ -488,7 +427,7 @@ class Thread {
      * @type {Node[]|undefined}
      * @private
      */
-    this.collapsedRange = this.getRangeContents();
+    this.collapsedRange = getElementRangeContents(this.startElement, this.getAdjustedEndElement());
 
     this.collapsedRange.forEach((el) => {
       // We use a class here because there can be elements in the comment that are hidden from the

src/js/processPage.js
@@ -443,9 +443,15 @@ function processComments(parser) {
  * @private
  */
 function processSections(parser, watchedSectionsRequest) {
-  parser.findHeadings().forEach((heading) => {
+  parser.findHeadings().forEach((heading, i, headings) => {
     try {
-      cd.sections.push(parser.createSection(heading, watchedSectionsRequest));
+      const [, level] = heading.tagName.match(/^H([1-6])$/);
+      const levelRegexp = new RegExp(`^H[1-${level}]$`);
+      const nextRelevantHeading = headings
+        .slice(i + 1)
+        .find((heading) => levelRegexp.test(heading.tagName));
+      const section = parser.createSection(heading, watchedSectionsRequest, nextRelevantHeading);
+      cd.sections.push(section);
     } catch (e) {
       if (!(e instanceof CdError)) {
         console.error(e);

src/js/util.js
@@ -994,3 +994,71 @@ export function getCommonGender(users) {
   }
   return commonGender;
 }
+
+/**
+ * Get all nodes between the two specified, including them. This works equally well if they are at
+ * different nesting levels. Descendants of nodes that are already included are not included.
+ *
+ * @param {Element} start
+ * @param {Element} end
+ * @returns {Element[]}
+ * @private
+ */
+export function getElementRangeContents(start, end) {
+  let commonAncestor;
+  for (let el = start; el; el = el.parentNode) {
+    if (el.contains(end)) {
+      commonAncestor = el;
+      break;
+    }
+  }
+
+  /*
+    Here we should equally account for all cases of the start and end item relative position.
+
+      <ul>         <!-- Say, may start anywhere from here... -->
+        <li></li>
+        <li>
+          <div></div>
+        </li>
+        <li></li>
+      </ul>
+      <div></div>  <!-- ...to here. And, may end anywhere from here... -->
+      <ul>
+        <li></li>
+        <li>
+          <div></div>
+        </li>
+        <li></li>  <-- ...to here. -->
+      </ul>
+   */
+  const rangeContents = [start];
+
+  // The start container could contain the end container and be different from it in the case with
+  // adjusted end items.
+  if (!start.contains(end)) {
+    const treeWalker = new ElementsTreeWalker(start);
+
+    while (treeWalker.currentNode.parentNode !== commonAncestor) {
+      while (treeWalker.nextSibling()) {
+        rangeContents.push(treeWalker.currentNode);
+      }
+      treeWalker.parentNode();
+    }
+    treeWalker.nextSibling();
+    while (!treeWalker.currentNode.contains(end)) {
+      rangeContents.push(treeWalker.currentNode);
+      treeWalker.nextSibling();
+    }
+    while (treeWalker.currentNode !== end) {
+      treeWalker.firstChild();
+      while (!treeWalker.currentNode.contains(end)) {
+        rangeContents.push(treeWalker.currentNode);
+        treeWalker.nextSibling();
+      }
+    }
+    rangeContents.push(end);
+  }
+
+  return rangeContents;
+}

commit da3d0f4f65e2d2a2ce2b89054deb2e65f757c487

Fix "Reply in section" button appearing under subsection heading
Fixes a bug introduced in 9b2b1bf0625e60c47393b6feb802b37ddd44a3bc.

Change-Id: Idca721646b4f4713b7be8b610f9b2cc5d2b18892

src/js/SectionSkeleton.js
@@ -167,10 +167,10 @@ class SectionSkeleton {
       elements = getElementRangeContents(this.headingElement, lastElement);
     }
     let hasSubsections = false;
-    elements.slice(1).some((el) => {
+    elements.slice(1).some((el, i) => {
       if (this.lastElementInFirstChunk === undefined && /^H[2-6]$/.test(el.tagName)) {
         hasSubsections = true;
-        this.lastElementInFirstChunk = el;
+        this.lastElementInFirstChunk = elements[i];
         return true;
       } else {
         return false;