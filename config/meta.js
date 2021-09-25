9d9a1ea2ba9633dd3b2b2264445f2bfe4de806f1
jwbth <jack.who.built.the.house@outlook.com>
2021-09-25 18:47:11 +0300

    Add Meta configuration file and URL
    Change-Id: Ibe8c7b65ef66120e436ecbc497ec8a63c55c72bc

---
+++ config/meta.js
@@ -0,0 +1,103 @@
+export default {
+	'messages': {
+		'sun': 'Sun',
+		'mon': 'Mon',
+		'tue': 'Tue',
+		'wed': 'Wed',
+		'thu': 'Thu',
+		'fri': 'Fri',
+		'sat': 'Sat',
+		'sunday': 'Sunday',
+		'monday': 'Monday',
+		'tuesday': 'Tuesday',
+		'wednesday': 'Wednesday',
+		'thursday': 'Thursday',
+		'friday': 'Friday',
+		'saturday': 'Saturday',
+		'jan': 'Jan',
+		'feb': 'Feb',
+		'mar': 'Mar',
+		'apr': 'Apr',
+		'may': 'May',
+		'jun': 'Jun',
+		'jul': 'Jul',
+		'aug': 'Aug',
+		'sep': 'Sep',
+		'oct': 'Oct',
+		'nov': 'Nov',
+		'dec': 'Dec',
+		'january': 'January',
+		'february': 'February',
+		'march': 'March',
+		'april': 'April',
+		'may_long': 'May',
+		'june': 'June',
+		'july': 'July',
+		'august': 'August',
+		'september': 'September',
+		'october': 'October',
+		'november': 'November',
+		'december': 'December',
+		'january-gen': 'January',
+		'february-gen': 'February',
+		'march-gen': 'March',
+		'april-gen': 'April',
+		'may-gen': 'May',
+		'june-gen': 'June',
+		'july-gen': 'July',
+		'august-gen': 'August',
+		'september-gen': 'September',
+		'october-gen': 'October',
+		'november-gen': 'November',
+		'december-gen': 'December',
+		'timezone-utc': 'UTC',
+		'parentheses': '($1)',
+		'parentheses-start': '(',
+		'parentheses-end': ')',
+		'word-separator': ' ',
+		'comma-separator': ', ',
+		'colon-separator': ': ',
+		'nextdiff': 'Newer edit →'
+	},
+	'contribsPage': 'Special:Contributions',
+	'timezone': 'UTC',
+	'useGlobalPreferences': true,
+	'unsignedTemplates': [
+		'Unsigned',
+		'Unsigned3',
+		'Unsigned2',
+		'Unsigned IP',
+		'Unsigned-ip',
+		'UnsignedIP'
+	],
+	'paragraphTemplates': [
+		'pb',
+		'Paragraph break'
+	],
+	'outdentTemplates': [
+		'outdent',
+		'Unindent',
+		'Od',
+		'OUTDENT'
+	],
+	'clearTemplates': [
+		'Clear',
+		'Br'
+	],
+	'templatesToExclude': [
+		'Moved'
+	],
+	'closedDiscussionTemplates': [
+		[
+			'Closed',
+			'Discussion top',
+			'Dt',
+			'Archive top'
+		],
+		[
+			'Discussion bottom',
+			'Archive bottom'
+		]
+	],
+	'signatureEndingRegexp': / \(talk\)/
+};
