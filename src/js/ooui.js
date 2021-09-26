408952102168d103369aaba4003ea0a7d9ede2a6
jwbth <jack.who.built.the.house@outlook.com>
2021-09-26 17:51:39 +0300

    Fix pointless confirmation of closing settings dialog
    It appeared after settings were saved and the close button was pressed.
    
    Change-Id: I19e93309e2720f6a01df69c3e268ba6f11f5b610

--- src/js/ooui.js
+++ src/js/ooui.js
@@ -191,7 +191,7 @@ export async function showConfirmDialog(message, options = {}) {
  */
 export function isDialogUnsaved(dialog) {
   const saveButton = dialog.actions.get({ actions: 'save' })[0];
-  return saveButton && !saveButton.isDisabled();
+  return saveButton && saveButton.isVisible() && !saveButton.isDisabled();
 }
 
 /**
