jwbth/convenient-discussions

commit 2fda1cba6f5d0c3031c77310bcb8f0b5c3131d68

Fix adding sections with absent headline input
Change-Id: Idadd263d1aa2b4bb56392b0c032cb6e2167302bf

src/js/CommentForm.js
@@ -3294,7 +3294,14 @@ class CommentForm {
           this.isSectionOpeningCommentEdited &&
           this.headlineInput.getValue() !== this.originalHeadline
         );
-        if (this.mode === 'addSection' || this.mode === 'addSubsection' || isHeadlineAltered) {
+
+        if (
+          // TODO: When there is no headline input, extract the headline from `== ==` markup.
+          (this.mode === 'addSection' && this.headlineinput) ||
+
+          this.mode === 'addSubsection' ||
+          isHeadlineAltered
+        ) {
           const headline = removeWikiMarkup(this.headlineInput.getValue());
           passedData.justWatchedSection = headline;
           let originalHeadline;

commit 0d309d3a679b4d875579a12bfe1ee93f063c5849

Fix not watching newly created topics
The bug was introduced in 2fda1cba6f5d0c3031c77310bcb8f0b5c3131d68.

Change-Id: Ib025a99385a7c2729a09f793349eb15d79993f50

src/js/CommentForm.js
@@ -3317,7 +3317,7 @@ class CommentForm {
 
         if (
           // TODO: When there is no headline input, extract the headline from `== ==` markup.
-          (this.mode === 'addSection' && this.headlineinput) ||
+          (this.mode === 'addSection' && this.headlineInput) ||
 
           this.mode === 'addSubsection' ||
           isHeadlineAltered