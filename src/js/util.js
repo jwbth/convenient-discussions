jwbth/convenient-discussions

commit 23f4caaf621fe1d0828072aef304256b972297ed

Improve mechanism to keep relative scroll position after parsing page
Don't save the position relative to floating elements.

Change-Id: I9946b9e48c23a2c20aa9a2d1db074f0b6a421d14

src/js/util.js
@@ -539,11 +539,17 @@ export function saveRelativeScrollPosition(switchToAbsolute = null, scrollY = wi
       scrollData.touchesBottom = true;
     } else if (scrollY !== 0 && cd.g.rootElement.getBoundingClientRect().top <= 0) {
       const treeWalker = new ElementsTreeWalker(cd.g.rootElement.firstElementChild);
-      while (true) {
-        if (!isInline(treeWalker.currentNode)) {
-          const rect = treeWalker.currentNode.getBoundingClientRect();
+      do {
+        const node = treeWalker.currentNode;
+
+        // Ignore elements with non-native classes - they can be floating.
+        if (
+          !isInline(node) &&
+          Array.from(node.classList).every((name) => /^(cd|ext-discussiontools|mw)-/.test(name))
+        ) {
+          const rect = node.getBoundingClientRect();
           if (rect.bottom >= 0 && rect.height !== 0) {
-            scrollData.element = treeWalker.currentNode;
+            scrollData.element = node;
             scrollData.elementTop = rect.top;
             if (treeWalker.firstChild()) {
               continue;
@@ -552,8 +558,7 @@ export function saveRelativeScrollPosition(switchToAbsolute = null, scrollY = wi
             }
           }
         }
-        if (!treeWalker.nextSibling()) break;
-      }
+      } while (treeWalker.nextSibling());
     }
   }
 }

commit 40053a994dfccd91ece3f27d16eeec187cd96caf

Fix saving relative scroll position
"continue;" line lead to "treeWalker.nextSibling();" being executed while it shouldn't. The bug was introduced in 23f4caaf621fe1d0828072aef304256b972297ed.

Change-Id: Ib1eefd1351b9d7c3419c5ba6ed1b3b6e60000d31

src/js/util.js
@@ -539,7 +539,7 @@ export function saveRelativeScrollPosition(switchToAbsolute = null, scrollY = wi
       scrollData.touchesBottom = true;
     } else if (scrollY !== 0 && cd.g.rootElement.getBoundingClientRect().top <= 0) {
       const treeWalker = new ElementsTreeWalker(cd.g.rootElement.firstElementChild);
-      do {
+      while (true) {
         const node = treeWalker.currentNode;
 
         // Ignore elements with non-native classes - they can be floating.
@@ -558,7 +558,8 @@ export function saveRelativeScrollPosition(switchToAbsolute = null, scrollY = wi
             }
           }
         }
-      } while (treeWalker.nextSibling());
+        if (!treeWalker.nextSibling()) break;
+      }
     }
   }
 }