jwbth/convenient-discussions

commit 92958ec01956f4c3d55cc7dee1544a900ffc819c

Apply minor improvements
Change-Id: Icd54df833f9458b2aab3dceffc1af427f31fa089

src/js/BootProcess.js
@@ -166,7 +166,7 @@ export default class BootProcess {
    * @returns {*}
    */
   data(name) {
-    return name ? this.passedData[name] : this.passedData;
+    return name ? this.passedData[name] || null : this.passedData;
   }
 
   /**

src/js/settings.js
@@ -571,7 +571,7 @@ export default {
    * @returns {*}
    */
   get(name) {
-    return name ? this.values[name] : this.values;
+    return name ? this.values[name] || null : this.values;
   },
 
   /**

src/js/Comment.js
@@ -69,7 +69,7 @@ function cleanUpThanks(data) {
 }
 
 /**
- * Get bounding client rectangle for a comment part.
+ * Get the bounding client rectangle for a comment part.
  *
  * @param {Element} el
  * @returns {object}

src/js/CommentForm.js
@@ -1869,9 +1869,10 @@ class CommentForm {
    */
   adjustLabels() {
     let formWidth = this.$element.width();
+    const additive = 7;
 
     if (this.$element.hasClass('cd-commentForm-short')) {
-      if (formWidth >= this.buttonsTotalWidthStandard + 7) {
+      if (formWidth >= this.buttonsTotalWidthStandard + additive) {
         this.$element.removeClass('cd-commentForm-short');
         this.submitButton.setLabel(this.submitButtonLabelStandard);
         this.previewButton.setLabel(cd.s('cf-preview'));
@@ -1879,24 +1880,19 @@ class CommentForm {
         this.cancelButton.setLabel(cd.s('cf-cancel'));
       }
     } else {
-      this.buttonsTotalWidthStandard = (
-        this.submitButton.$element.outerWidth(true) +
-        (
-          this.previewButton.$element.is(':visible') ?
-            this.previewButton.$element.outerWidth(true) :
-            0
-        ) +
-        // Users may hide the view changes button by any kind of a plugin.
-        (
-          this.viewChangesButton.$element.is(':visible') ?
-            this.viewChangesButton.$element.outerWidth(true) :
-            0
-        ) +
-        this.advancedButton.$element.outerWidth(true) +
-        this.helpPopupButton.$element.outerWidth(true) +
-        this.cancelButton.$element.outerWidth(true)
-      );
-      if (formWidth < this.buttonsTotalWidthStandard + 7) {
+      this.buttonsTotalWidthStandard = [
+        'submitButton',
+        'previewButton',
+        'viewChangesButton',
+        'cancelButton',
+        'advancedButton',
+        'helpPopupButton',
+        'settingsButton',
+      ]
+        .map((name) => this[name].$element)
+        .filter(($el) => $el.is(':visible'))
+        .reduce((width, $el) => width + $el.outerWidth(true), 0);
+      if (formWidth < this.buttonsTotalWidthStandard + additive) {
         this.$element.addClass('cd-commentForm-short');
         this.submitButton.setLabel(this.submitButtonLabelShort);
         this.previewButton.setLabel(cd.s('cf-preview-short'));

src/js/CommentSkeleton.js
@@ -1409,7 +1409,7 @@ class CommentSkeleton {
    * @returns {boolean}
    */
   static isDtId(string) {
-    return string?.startsWith('c-');
+    return Boolean(string?.startsWith('c-'));
   }
 
   /**

src/js/Section.js
@@ -359,7 +359,7 @@ class Section extends SectionSkeleton {
    * @returns {boolean}
    */
   canEditFirstComment() {
-    return (
+    return Boolean(
       this.isActionable &&
       this.comments.length &&
       this.comments[0].isOpeningSection &&

src/js/addCommentLinks.js
@@ -46,8 +46,7 @@ async function prepare() {
   if (!settings.get('useTopicSubscription')) {
     // Loading the subscriptions is not critical, as opposed to messages, so we catch the possible
     // error, not letting it be caught by the try/catch block.
-    subscriptions.load(true);
-    subscriptions.getLoadRequest().catch((e) => {
+    subscriptions.load(true).catch((e) => {
       console.warn('Couldn\'t load the settings from the server.', e);
     });
     requests.push(subscriptions.getLoadRequest());

src/js/apiWrappers.js
@@ -238,14 +238,12 @@ export async function setVisits(visits) {
  * @param {boolean} [reuse=false] Whether to reuse a cached userinfo request.
  * @returns {Promise.<object>}
  */
-export async function getLegacySubscriptions(reuse = false) {
+export function getLegacySubscriptions(reuse = false) {
   const isOptionSet = mw.user.options.get(cd.g.SUBSCRIPTIONS_OPTION_NAME) !== null;
   const promise = controller.getBootProcess()?.isPageFirstParsed() && !isOptionSet ?
     Promise.resolve({}) :
     getUserInfo(reuse).then((options) => options.subscriptions);
-  const registry = await promise;
-
-  return registry;
+  return promise;
 }
 
 /**
@@ -762,10 +760,8 @@ export async function getDtSubscriptions(ids) {
       action: 'discussiontoolsgetsubscriptions',
       commentname: nextIds,
     }).catch(handleApiReject);
-
     Object.assign(subscriptions, resp.subscriptions);
   }
-
   return subscriptions;
 }
 

commit f7452f591f4e532854eb6ba8d21cc08652f35f26

Fix settings#get, BootProcess#data
The regression was introduced in 92958ec01956f4c3d55cc7dee1544a900ffc819c.

Change-Id: Ie5c3b057971f2088835a86a952509dec9dddddba

src/js/BootProcess.js
@@ -166,7 +166,7 @@ export default class BootProcess {
    * @returns {*}
    */
   data(name) {
-    return name ? this.passedData[name] || null : this.passedData;
+    return name ? this.passedData[name] ?? null : this.passedData;
   }
 
   /**

src/js/settings.js
@@ -571,7 +571,7 @@ export default {
    * @returns {*}
    */
   get(name) {
-    return name ? this.values[name] || null : this.values;
+    return name ? this.values[name] ?? null : this.values;
   },
 
   /**