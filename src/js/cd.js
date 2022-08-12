7ed0379d731fd03c0919c497464203f44b9390e7
jwbth <jack.who.built.the.house@outlook.com>
2022-08-12 13:29:53 +0300

    Simplify cd module
    Change-Id: I7d3fd37c07e8fb24cadcaf240e061d59fe0664c8

--- src/js/cd.js
+++ src/js/cd.js
@@ -5,18 +5,12 @@
  * @module cd
  */
 
-// Window or worker context
-const context = typeof window === 'undefined' ? self : window;
-
 /**
  * The main script object, globally available (the modules use the {@link module:cd cd} alias).
  *
  * @namespace convenientDiscussions
  * @global
  */
-context.convenientDiscussions ||= {};
-if (typeof context.convenientDiscussions !== 'object') {
-  context.convenientDiscussions = {};
-}
+self.convenientDiscussions ||= {};
 
-export default context.convenientDiscussions;
+export default self.convenientDiscussions;
