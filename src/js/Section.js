293c7e2c21a5476a296a74406fca54b6ba2c9e1a
jwbth <jack.who.built.the.house@outlook.com>
2023-01-05 14:56:16 +0300

    Fix occasional error when replying to last comment on page
    Change-Id: I7481cacddd85c1e39dabb530a5b6ac8dab907458

--- src/js/Section.js
+++ src/js/Section.js
@@ -1481,7 +1481,7 @@ class Section extends SectionSkeleton {
         equalSignsPattern +
         '[^=].*=+[ \\t\\x01\\x02]*\\n'
       )) ||
-      codeFromSection.match(new RegExp(
+      adjustedCodeFromSection.match(new RegExp(
         '(' +
         mw.util.escapeRegExp(fullHeadingMatch) +
         '[^]*$)'
@@ -1503,7 +1503,7 @@ class Section extends SectionSkeleton {
 
         '[^=].*=+[ \\t\\x01\\x02]*\n'
       )) ||
-      codeFromSection.match(new RegExp(
+      adjustedCodeFromSection.match(new RegExp(
         '(' +
         mw.util.escapeRegExp(fullHeadingMatch) +
         '[^]*$)'
