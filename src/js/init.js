162b9fba17ec7fb5da884bb349b5487d68470077
jwbth <jack.who.built.the.house@outlook.com>
2023-10-05 02:04:38 +0400

    Fix strict equality operator
    Change-Id: Ifa6512ff9b0274e275a278196948aa764c1f5cf6

--- src/js/init.js
+++ src/js/init.js
@@ -870,7 +870,7 @@ export default {
 
     // For Timeless, Vector-2022 skins
     cd.g.bodyScrollPaddingTop = parseFloat($('html, body').css('scroll-padding-top')) || 0;
-    if (cd.g.skin = 'timeless') {
+    if (cd.g.skin === 'timeless') {
       cd.g.bodyScrollPaddingTop = cd.g.bodyScrollPaddingTop - 5;
     }
   },
