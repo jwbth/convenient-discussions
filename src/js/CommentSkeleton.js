8a9ecf55009274ac2fb76fa7733946643dce67da
jwbth <jack.who.built.the.house@outlook.com>
2024-01-31 01:01:29 +0400

    Fix determining highlightable comment parts
    Some markup change or something caused floating images to be interpreted as highlightable elements.
    
    Change-Id: I2ea75e4cdcbbd115c67fdff2abd220be8337fec8

--- src/js/CommentSkeleton.js
+++ src/js/CommentSkeleton.js
@@ -1091,6 +1091,7 @@ class CommentSkeleton {
       !isHeadingNode(el) &&
       !isMetadataNode(el) &&
       !cd.g.noHighlightClasses.some((name) => el.classList.contains(name)) &&
+      !(el.tagName === 'FIGURE' && /^(mw:File\/(Thumb|Frame))/.test(el.getAttribute('typeof'))) &&
 
       // Can't access stylesheets from the worker context, so we do it only in
       // Comment#reviewHighlightables, and here we look at the style attribute only.
