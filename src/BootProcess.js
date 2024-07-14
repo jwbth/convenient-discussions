jwbth/convenient-discussions

commit 20bf0954c13319d2127952403bf06e7bc34acc3f

Update page subtitle and categories on page reload
This removes the need to update the page's subtitle by our means, as I did in 44e6a0f5965524e50f28cf9567c6b3e6c9caee8f.

Change-Id: Ie5a5f29a26a58a19939687f045a9a6ef3716a826

src/BootProcess.js
@@ -23,7 +23,6 @@ import updateChecker from './updateChecker';
 import userRegistry from './userRegistry';
 import { handleApiReject, saveOptions } from './utils-api';
 import { definedAndNotNull, sleep } from './utils-general';
-import { formatDate } from './utils-timestamp';
 import { wrapHtml } from './utils-window';
 import visits from './visits';
 
@@ -280,8 +279,8 @@ class BootProcess {
       notifications.init();
       Parser.init();
     }
-    controller.setup(this.passedData.html);
-    toc.setup(this.passedData.toc, this.passedData.hidetoc);
+    controller.setup(this.passedData.parseData?.text);
+    toc.setup(this.passedData.parseData?.sections, this.passedData.parseData?.hidetoc);
 
     /**
      * Collection of all comments on the page ordered the same way as in the DOM.
@@ -551,55 +550,6 @@ class BootProcess {
   }
 
   /**
-   * Update DiscussionTools' "Latest comment" text in the page subtitle if Visual Enhancements are
-   * enabled. The differences between DT and CD:
-   * * CD uses user's settings for the timestamp format while DT always uses relative timestamp;
-   * * CD auto-updates relative timestamps (e.g. "less than a minute ago" → "1 minute ago");
-   * * CD uses "less than a minute ago" whereas DT uses "just now" for relative dates less than a
-   *   minute ago.
-   *
-   * @private
-   */
-  async updateDtLatestComment() {
-    if (!cd.g.isDtVisualEnhancementsEnabled) return;
-
-    const $latestComment = $('.ext-discussiontools-init-pageframe-latestcomment');
-    if (!$latestComment) return;
-
-    await controller.getApi().loadMessagesIfMissing([
-      'discussiontools-pageframe-latestcomment',
-      'discussiontools-pageframe-latestcomment-notopic',
-    ]);
-
-    const latestComment = Comment.getLatest(commentRegistry.getAll());
-    if (latestComment) {
-      const $latestCommentLink = $('<a>')
-        .attr('href', `#${latestComment.dtId || latestComment.id}`)
-        .text(formatDate(latestComment.date));
-      if (latestComment.section) {
-        const $sectionLink = $('<a>')
-          .attr('href', '#' + latestComment.section.id)
-          .text(latestComment.section.headline);
-        $latestComment.msg(
-          'discussiontools-pageframe-latestcomment',
-          $latestCommentLink,
-          latestComment.author.getName(),
-          $sectionLink
-        );
-      } else {
-        $latestComment.msg(
-          'discussiontools-pageframe-latestcomment-notopic',
-          $latestCommentLink,
-          latestComment.author.getName()
-        )
-      }
-      (new LiveTimestamp($latestCommentLink[0], latestComment.date, false)).init();
-    } else {
-      $latestComment.empty();
-    }
-  }
-
-  /**
    * Log debug data to the console.
    *
    * @private
@@ -723,9 +673,9 @@ class BootProcess {
       }
     }
 
-    if (this.passedData.html) {
+    if (this.passedData.parseData?.text) {
       debug.startTimer('update page contents');
-      controller.updatePageContents();
+      controller.updatePageContents(this.passedData.parseData);
       debug.stopTimer('update page contents');
     }
 
@@ -797,8 +747,6 @@ class BootProcess {
       }
 
       pageNav.setup(this);
-
-      this.updateDtLatestComment();
 
       if (this.firstRun) {
         controller.addEventListeners();

src/controller.js
@@ -1199,17 +1199,7 @@ export default {
     });
 
     try {
-      const parseData = await cd.page.parse(null, false, true);
-      bootProcess.passedData.html = parseData.text;
-      bootProcess.passedData.toc = parseData.sections;
-      bootProcess.passedData.hideToc = parseData.hidetoc;
-      mw.config.set({
-        wgRevisionId: parseData.revid,
-        wgCurRevisionId: parseData.revid,
-      });
-      mw.loader.load(parseData.modules);
-      mw.loader.load(parseData.modulestyles);
-      mw.config.set(parseData.jsconfigvars);
+      bootProcess.passedData.parseData = await cd.page.parse(null, false, true);
     } catch (e) {
       this.hideLoadingOverlay();
       if (bootProcess.passedData.wasCommentFormSubmitted) {
@@ -1252,10 +1242,29 @@ export default {
   },
 
   /**
-   * _For internal use._ Update the page's HTML.
+   * _For internal use._ Update the page's HTML and certain configuration values.
+   *
+   * @param {object} parseData
    */
-  updatePageContents() {
+  updatePageContents(parseData) {
     this.$content.children('.mw-parser-output').first().replaceWith(this.$root);
+
+    mw.util.clearSubtitle();
+    mw.util.addSubtitle(parseData.subtitle);
+
+    if ($('#catlinks').length) {
+      const $categories = $($.parseHTML(parseData.categorieshtml));
+      mw.hook('wikipage.categories').fire($categories);
+      $('#catlinks').replaceWith($categories);
+    }
+
+    mw.config.set({
+      wgRevisionId: parseData.revid,
+      wgCurRevisionId: parseData.revid,
+    });
+    mw.loader.load(parseData.modules);
+    mw.loader.load(parseData.modulestyles);
+    mw.config.set(parseData.jsconfigvars);
   },
 
   /**

src/pageRegistry.js
@@ -485,7 +485,7 @@ export class Page {
       disabletoc: cd.g.skin === 'vector-2022',
       useskin: cd.g.skin,
       redirects: true,
-      prop: ['text', 'revid', 'modules', 'jsconfigvars', 'sections'],
+      prop: ['text', 'revid', 'modules', 'jsconfigvars', 'sections', 'subtitle', 'categorieshtml'],
       parsoid: cd.g.isParsoidUsed,
       ...cd.g.apiErrorFormatHtml,
     };

commit e9e07fee7d8a65cbcd0e9f937e144985de5dbd82

Fix submitting "Add section" form
The "Add topic" button was not restored. The subscription button was not added (broken in 20bf0954c13319d2127952403bf06e7bc34acc3f when mw.config.set(parseData.jsconfigvars); was moved). Tear down the comment form when rescuing it as well (so it's not torn down only when submitting anything other than an "Add section" form).

Change-Id: I3ce9ac819b327fd6522dc5c367c697244453ffbc

src/BootProcess.js
@@ -122,7 +122,7 @@ function processAndRemoveDtElements(elements, bootProcess) {
  *   time for it to be saved to the server.
  * @property {string} [justUnwatchedSection] Section just unwatched so that there could be not
  *   enough time for it to be saved to the server.
- * @property {boolean} [wasCommentFormSubmitted] Did the user just submit a comment form.
+ * @property {CommentForm} [submittedCommentForm] Comment form the user just submitted.
  */
 
 /**
@@ -525,9 +525,12 @@ class BootProcess {
         // sleep() for Firefox, as above
         sleep().then(() => {
           // A tricky case with flashing is when a comment is in a collapsed thread. In this case,
-          // we must use Comment#scrollTo to make sure it is flashed when the thread is uncollapsed
-          // by clicking a link in the notification.
-          const flashOne = this.passedData.wasCommentFormSubmitted || this.passedData.pushState;
+          // we must use Comment#scrollTo() to make sure it is flashed when the thread is
+          // uncollapsed by clicking a link in the notification.
+          const flashOne = Boolean(
+            this.passedData.submittedCommentForm ||
+            this.passedData.pushState
+          );
           comments[0].scrollTo({
             smooth: false,
             pushState: this.passedData.pushState,

src/Comment.js
@@ -3012,12 +3012,13 @@ class Comment extends CommentSkeleton {
   }
 
   /**
-   * Remove a comment form {@link CommentForm#getTarget targeted} at this comment from the page.
+   * Clean up traces of a comment form {@link CommentForm#getTarget targeted} at this comment from
+   * the page.
    *
    * @param {string} mode
    * @param {import('./CommentForm').default} commentForm
    */
-  removeCommentFormFromPage(mode, commentForm) {
+  cleanUpCommentFormTraces(mode, commentForm) {
     if (mode === 'reply') {
       this.subitemList.remove('replyForm');
       this.scrollIntoView('top');

src/CommentForm.js
@@ -3066,7 +3066,8 @@ class CommentForm {
       // Add the created section to the subscription list or change the headline for legacy
       // subscriptions.
       if (
-        // FIXME: sections added with no headline (actually comments added to the preceding section)
+        // FIXME: fix behavior for sections added with no headline (that are, in fact, comments
+        // added to the preceding section)
         this.mode === 'addSection' ||
         (
           !this.useTopicSubscription &&
@@ -3183,7 +3184,7 @@ class CommentForm {
     // be watched/unwatched using a checkbox in a form just sent. The server doesn't manage to
     // update the value quickly enough, so it returns the old value, but we must display the new
     // one.
-    const bootData = { wasCommentFormSubmitted: true };
+    const bootData = { submittedCommentForm: this };
 
     if (this.subscribeCheckbox) {
       this.updateSubscriptionStatus(editTimestamp, commentCode, bootData);
@@ -3205,9 +3206,7 @@ class CommentForm {
     if (!doDelete) {
       // Generate an ID for the comment to jump to.
       bootData.commentIds = [
-        this.mode === 'edit' ?
-          this.target.id :
-          this.generateFutureCommentId(editTimestamp)
+        this.mode === 'edit' ? this.target.id : this.generateFutureCommentId(editTimestamp),
       ];
     }
 
@@ -3250,13 +3249,13 @@ class CommentForm {
    * Remove the comment form elements and restore the page elements that were hidden. Remove
    * properties of other objects related to the form. Close all form operations and remove all
    * references to the form.
-   *
-   * @private
    */
   teardown() {
     this.operations.closeAll();
-    this.target.removeCommentFormFromPage(this.mode, this);
-    this.$element.remove();
+    if (this.$element[0].isConnected) {
+      this.target.cleanUpCommentFormTraces(this.mode, this);
+      this.$element.remove();
+    }
     this.unregister();
     this.emit('teardown');
     this.torndown = true;
@@ -3265,12 +3264,13 @@ class CommentForm {
   /**
    * Remove all outside references to the form and unload it from the session data thus making it
    * not appear after a page reload. A form may be unregistered without being torn down (but not
-   * vice versa); for example, submitted forms and forms that can't be restored after a page reload
-   * for whatever reason.
+   * vice versa) - when it is submitted.
    *
    * @private
    */
   unregister() {
+    if (!this.registered) return;
+
     this.constructor.forgetOnTarget(this.target, this.mode);
 
     // Popups can be placed outside the form element, so they need to be torn down whenever the form
@@ -3864,7 +3864,7 @@ class CommentForm {
    * @returns {object}
    */
   rescue() {
-    this.unregister();
+    this.teardown();
 
     return {
       headline: this.headlineInput?.getValue(),

src/Section.js
@@ -1092,11 +1092,12 @@ class Section extends SectionSkeleton {
   }
 
   /**
-   * Remove a comment form {@link CommentForm#getTarget targeted} at this section from the page.
+   * Clean up traces of a comment form {@link CommentForm#getTarget targeted} at this section from
+   * the page.
    *
    * @param {string} mode
    */
-  removeCommentFormFromPage(mode) {
+  cleanUpCommentFormTraces(mode) {
     if (mode === 'replyInSection') {
       this.replyButton.show();
       this.$replyButtonWrapper.removeClass('cd-replyButtonWrapper-hasCommentForm');
@@ -1690,7 +1691,7 @@ class Section extends SectionSkeleton {
   }
 
   /**
-   * Get the section used to subscribe to if available.
+   * Get the section used to subscribe to new comments in this section if available.
    *
    * @returns {?Section}
    */

src/controller.js
@@ -1213,7 +1213,7 @@ export default {
       bootProcess.passedData.parseData = await cd.page.parse(null, false, true);
     } catch (e) {
       this.hideLoadingOverlay();
-      if (bootProcess.passedData.wasCommentFormSubmitted) {
+      if (bootProcess.passedData.submittedCommentForm) {
         throw e;
       } else {
         mw.notify(cd.s('error-reloadpage'), { type: 'error' });
@@ -1221,6 +1221,14 @@ export default {
         return;
       }
     }
+
+    mw.loader.load(bootProcess.passedData.parseData.modules);
+    mw.loader.load(bootProcess.passedData.parseData.modulestyles);
+
+    // It would be perhaps more correct to set the config variables in
+    // controller.updatePageContents(), but we need wgDiscussionToolsPageThreads from there before
+    // that.
+    mw.config.set(bootProcess.passedData.parseData.jsconfigvars);
 
     // Get IDs of unseen comments. This is used to arrange that they will still be there after
     // replying on or refreshing the page.
@@ -1233,10 +1241,11 @@ export default {
 
     this.emit('startReload');
 
-    // Just submitted "Add section" form (it is outside of the .$root element). Forms that should
-    // stay are detached above.
-    this.$addSectionButtonContainer?.remove();
-    $('.cd-commentForm-addSection').remove();
+    // Just submitted "Add section" form (it is outside of the .$root element, so we must remove it
+    // here). Forms that should stay are detached above.
+    if (bootProcess.passedData.submittedCommentForm?.getMode() === 'addSection') {
+      bootProcess.passedData.submittedCommentForm.teardown();
+    }
 
     this.reset();
 
@@ -1272,9 +1281,6 @@ export default {
       wgRevisionId: parseData.revid,
       wgCurRevisionId: parseData.revid,
     });
-    mw.loader.load(parseData.modules);
-    mw.loader.load(parseData.modulestyles);
-    mw.config.set(parseData.jsconfigvars);
   },
 
   /**

src/pageRegistry.js
@@ -830,7 +830,7 @@ export class Page {
       }
       $('#ca-addsection').addClass('selected');
       $('#ca-view').removeClass('selected');
-      this.addSectionForm.on('unregister', () => {
+      this.addSectionForm.on('teardown', () => {
         $('#ca-addsection').removeClass('selected');
         $('#ca-view').addClass('selected');
       });
@@ -838,7 +838,8 @@ export class Page {
   }
 
   /**
-   * Add a comment form {@link CommentForm#getTarget targeted} at this page to the page.
+   * Clean up traces of a comment form {@link CommentForm#getTarget targeted} at this page to the
+   * page.
    *
    * @param {string} mode
    * @param {import('./CommentForm').default} commentForm
@@ -854,7 +855,7 @@ export class Page {
   /**
    * Remove a comment form {@link CommentForm#getTarget targeted} at this page from the page.
    */
-  removeCommentFormFromPage() {
+  cleanUpCommentFormTraces() {
     if (!this.exists()) {
       controller.$content
         // In case DT's new topic tool is enabled. This is responsible for correct styles being set.

src/updateChecker.js
@@ -748,7 +748,7 @@ const updateChecker = {
             undefined,
           (
             (
-              bootProcess.passedData.wasCommentFormSubmitted &&
+              bootProcess.passedData.submittedCommentForm &&
               bootProcess.passedData.commentIds?.[0]
             ) ||
             undefined