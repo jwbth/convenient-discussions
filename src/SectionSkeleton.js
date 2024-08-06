5f7705ed3a00d4ed40897f2049bc6aa73cd12477
jwbth <jack.who.built.the.house@outlook.com>
2024-08-07 03:55:59 +0400

    Undo "Make compatible with autonum gadget"
    This is not needed. Partially reverts 742800380c53c1d259808587b5fde40fb6aa6071.
    
    Change-Id: Ib8c2d88b4ed885dabb762d128191667d04695b0b

--- src/SectionSkeleton.js
+++ src/SectionSkeleton.js
@@ -287,9 +287,6 @@ class SectionSkeleton {
       // Was removed in 2021, see T284921. Keep this for some time.
       'mw-headline-number',
 
-      // https://www.mediawiki.org/wiki/Topic:Y90udqpc29ehe599
-      'tpl-autonum-num',
-
       'mw-editsection-like',
       ...cd.config.excludeFromHeadlineClasses,
     ];
