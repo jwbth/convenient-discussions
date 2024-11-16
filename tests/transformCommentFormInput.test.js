9e8c3433c0e6af7415649d9fc7fb83e175f6b4dd
jwbth <jack.who.built.the.house@outlook.com>
2024-11-16 20:03:18 +0400

    Fix transforming list markup to tags when in a template (2)
    Follow-up to 8d1466089a19ab60ab8a1c61f919b50bc89d328f.
    
    Change-Id: Ifb6166122eadfcaa204466fb8f510ba843d71a42

--- tests/transformCommentFormInput.test.js
+++ tests/transformCommentFormInput.test.js
@@ -473,6 +473,12 @@ describe('Tags and templates', () => {
     firstCommentReplyForm
   );
   testWithData(
+    'List in a template without a newline before a named parameter\'s content, but there is also another parameter starting with #',
+    'Quoted list:\n{{quote|link=#Section|1=* Item 1.\n** Subitem 1.\n* Item 2.\n}}\nEnd.',
+    ': Quoted list:{{quote|link=#Section|1=<ul><li>Item 1.<ul><li>Subitem 1.</li></ul></li><li>Item 2.</li></ul>}}End. ~~~~\n',
+    firstCommentReplyForm
+  );
+  testWithData(
     'Gallery tag',
     'Start.\n<gallery>\nGallery.\n</gallery>\nEnd.',
     ': Start.\n<gallery>\nGallery.\n</gallery>\n: End. ~~~~\n',
