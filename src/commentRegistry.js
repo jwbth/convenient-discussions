jwbth/convenient-discussions

commit cfb4e8cbcdfa021ab5ad8b3e257fbec4427da169

Apply minor improvements
Change-Id: I2cfe34b62edac679b248650abebb3ed2bec2f9f5

src/commentRegistry.js
@@ -806,7 +806,7 @@ export default {
           if (!comment.isSelected) {
             this.resetSelectedComment();
             comment.setSelected(true);
-            this.emit('selected', comment);
+            this.emit('select', comment);
           }
         } else {
           this.resetSelectedComment();

config/s-ru.js
@@ -75,18 +75,18 @@ export default {
 		'discussiontools-newtopicssubscription-notify-subscribed-body': '{{GENDER:|Вы}} будете получать уведомления о создании новых тем на этой странице.',
 		'discussiontools-newtopicssubscription-notify-unsubscribed-title': '{{GENDER:|Вы}} отписались.',
 		'discussiontools-newtopicssubscription-notify-unsubscribed-body': '{{GENDER:|Вы}} больше не будете получать уведомления о создании новых тем на этой странице.',
-		'visualeditor-educationpopup-dismiss': 'Окей, понятно'
+		'visualeditor-educationpopup-dismiss': 'Окей, понятно',
 	},
 
 	specialPageAliases: {
 		'Contributions': 'Вклад',
 		'Diff': 'Изменения',
-		'PermanentLink': 'Постоянная_ссылка'
+		'PermanentLink': 'Постоянная_ссылка',
 	},
 
 	substAliases: [
 		'подстановка:',
-		'подст:'
+		'подст:',
 	],
 
 	timezone: 'UTC',
@@ -96,44 +96,44 @@ export default {
 	unsignedTemplates: [
 		'Нет подписи',
 		'Unsigned',
-		'UnsignedIP'
+		'UnsignedIP',
 	],
 
 	pairQuoteTemplates: [
 		[
-			'Начало цитаты'
+			'Начало цитаты',
 		],
 		[
-			'Конец цитаты'
-		]
+			'Конец цитаты',
+		],
 	],
 
 	paragraphTemplates: [
 		'pb',
 		'Абзац',
-		'Paragraph break'
+		'Paragraph break',
 	],
 
 	outdentTemplates: [
 		'od',
-		'Outdent'
+		'Outdent',
 	],
 
 	clearTemplates: [
-		'Clear'
+		'Clear',
 	],
 
 	noSignatureTemplates: [
 		'Перенесено с',
-		'Перенесено на'
+		'Перенесено на',
 	],
 
 	closedDiscussionTemplates: [
 		[
 			'Закрыто',
-			'Closed'
+			'Closed',
 		],
-		[]
+		[],
 	],
 	signatureEndingRegexp: / \(обсуждение\)/,
 

config/w-fr.js
@@ -75,12 +75,12 @@ export default {
 		'discussiontools-newtopicssubscription-notify-subscribed-body': '{{GENDER:|}}Vous recevrez des notifications lorsque de nouveaux sujets seront lancés sur cette page.',
 		'discussiontools-newtopicssubscription-notify-unsubscribed-title': 'Vous êtes désabonné{{GENDER:||e}}.',
 		'discussiontools-newtopicssubscription-notify-unsubscribed-body': '{{GENDER:|}}Vous ne recevrez plus de notifications lorsque de nouveaux sujets sont lancés sur cette page.',
-		'visualeditor-educationpopup-dismiss': 'C’est bon, allons-y'
+		'visualeditor-educationpopup-dismiss': 'C’est bon, allons-y',
 	},
 	'specialPageAliases': {
 		'Contributions': 'Contributions',
 		'Diff': 'Diff',
-		'PermanentLink': 'Lien_permanent'
+		'PermanentLink': 'Lien_permanent',
 	},
 	'thumbAliases': [
 		'мини',
@@ -99,19 +99,19 @@ export default {
 		'NS',
 		'Non signé2',
 		'Non signé 2',
-		'Unsigned2'
+		'Unsigned2',
 	],
 	'pairQuoteTemplates': [
 		[
 			'Début citation bloc',
 			'Début de citation',
 			'Citation début',
-			'Début citation'
+			'Début citation',
 		],
 		[
 			'Fin citation bloc',
 			'Fin de citation',
-			'Fin citation'
+			'Fin citation',
 		]
 	],
 	'outdentTemplates': [
@@ -120,10 +120,10 @@ export default {
 		'Retour chariot',
 		'RI:',
 		'Ri',
-		'Outdent'
+		'Outdent',
 	],
 	'clearTemplates': [
-		'Clear'
+		'Clear',
 	],
 	'reflistTalkTemplates': [
 		'Références discussion',
@@ -134,7 +134,7 @@ export default {
 	'noSignatureClasses': [
 		'NavFrame',
   ],
-	'signatureEndingRegexp': / \(discuter\)/
+	'signatureEndingRegexp': / \(discuter\)/,
 };
 
 let styles;

src/CommentSource.js
@@ -314,11 +314,9 @@ class CommentSource {
       .maskSensitiveCode()
       .withText((text, textMasker) => {
         this.headingMatch = text.match(/(^[^]*(?:^|\n))((=+)(.*)\3[ \t\x01\x02]*\n)/);
-        if (this.headingMatch) {
-          this.headingMatch.forEach((group, i) => {
-            this.headingMatch[i] = textMasker.unmaskText(group);
-          });
-        }
+        this.headingMatch?.forEach((group, i) => {
+          this.headingMatch[i] = textMasker.unmaskText(group);
+        });
         return text;
       })
       .unmask()

src/LiveTimestamp.js
@@ -143,7 +143,7 @@ class LiveTimestamp {
     this.improvedTimestamps.forEach((timestamp) => {
       timestamp.update();
     });
-    this.emit('updateimproved');
+    this.emit('updateImproved');
   }
 
   /**

src/Section.less
@@ -88,7 +88,7 @@ ol .cd-section-button {
       cursor: default;
     }
 
-    margin-inline: 0 1.75em;
+    margin-inline: 0 2em;
 
     &:last-child {
       margin-inline-end: 0;

src/commentFormRegistry.js
@@ -59,8 +59,8 @@ export default {
       })
       .on('resize', this.adjustLabels.bind(this));
     commentRegistry
-      .on('selected', this.toggleQuoteButtonsHighlighting.bind(this, true))
-      .on('unselected', this.toggleQuoteButtonsHighlighting.bind(this, false));
+      .on('select', this.toggleQuoteButtonsHighlighting.bind(this, true))
+      .on('unselect', this.toggleQuoteButtonsHighlighting.bind(this, false));
   },
 
   /**

commit 735a751aac732fb2d15739eaa75f86ed2400fc65

commentRegistry: Fix "unselect" event name
The regression happened in cfb4e8cbcdfa021ab5ad8b3e257fbec4427da169.

Change-Id: I06e39b73c7189cc41a32d370ec35e83af58a30c9

src/commentRegistry.js
@@ -814,7 +814,7 @@ export default {
     const comment = this.items.find((comment) => comment.isSelected);
     if (comment) {
       comment.setSelected(false);
-      this.emit('unselected', comment);
+      this.emit('unselect', comment);
     }
   },
 