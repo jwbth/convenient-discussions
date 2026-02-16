4aaceec9d578ba6406176f96efd69d5a4ef3c987
jwbth <jack.who.built.the.house@outlook.com>
2026-02-16 12:19:45 +0400

    test: Get rid of redundant configs in Vitest
    Change-Id: Ieeb3ab57f1e74d1743058918a5ee46e429348d75

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,9 +1,4 @@
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-
 import { defineConfig } from 'vitest/config'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
 	test: {
@@ -11,23 +6,13 @@ export default defineConfig({
 		environment: 'jsdom',
 		setupFiles: ['./tests/setup.js'],
 		include: ['tests/**/*.test.js'],
-		alias: [
-			{
-				find: /.*\.(css|less)(\?inline)?$/,
-				replacement: path.resolve(__dirname, 'tests/styleMock.js'),
-			},
-			{
-				find: /.*worker-gate.*/,
-				replacement: path.resolve(__dirname, 'tests/workerMock.js'),
-			},
-		],
 	},
-	define: {
-		IS_DEV: true,
-		IS_STAGING: false,
-		IS_SINGLE: false,
-		SINGLE_CONFIG_FILE_NAME: 'undefined',
-		SINGLE_LANG_CODE: 'undefined',
-		CACHE_BUSTER: JSON.stringify('test-cache-buster'),
-	},
+	// define: {
+	// 	IS_DEV: true,
+	// 	IS_STAGING: false,
+	// 	IS_SINGLE: false,
+	// 	SINGLE_CONFIG_FILE_NAME: 'undefined',
+	// 	SINGLE_LANG_CODE: 'undefined',
+	// 	CACHE_BUSTER: JSON.stringify('test-cache-buster'),
+	// },
 })
