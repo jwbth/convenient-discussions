7b1947b216d9bd2a2f82f4f165f92e5fc1c33bef
jwbth <jack.who.built.the.house@outlook.com>
2026-03-13 12:26:09 +0400

    test: Update comment-overlay.spec.js to test for two comments
    Change-Id: I9902852bb38ccd312f6c063673a6052331fe1c10

--- e2e/comment-overlay.spec.js
+++ e2e/comment-overlay.spec.js
@@ -3,33 +3,23 @@ import { test, expect } from '@playwright/test'
 
 import { setupConvenientDiscussions } from './helpers/test-utils.js'
 
-test.describe('First comment overlay', () => {
+test.describe('Comment overlay', () => {
 	test.beforeEach(async ({ page }) => {
 		await setupConvenientDiscussions(page)
 	})
 
 	test('Overlay should appear when hovering the first comment', async ({ page }) => {
-		const firstCommentPart = page.locator('.cd-comment-part-first').first()
+		const firstCommentPart = page.locator('.cd-comment-part-first').nth(0)
 
-		// Get layers info for first and second comment
 		const debugInfo = await page.evaluate(() => {
 			const c1 = window.convenientDiscussions.comments[0]
-			const c2 = window.convenientDiscussions.comments[1]
-
-			// Force layer configuration
 			c1.configureLayers()
-			c2.configureLayers()
 
 			return {
 				c1: {
 					container: c1.layers.getContainer().tagName + '.' + c1.layers.getContainer().className,
 					offset: c1.layers.getContainerOffset(),
 					isVisible: window.getComputedStyle(c1.layers.getContainer()).display !== 'none',
-				},
-				c2: {
-					container: c2.layers.getContainer().tagName + '.' + c2.layers.getContainer().className,
-					offset: c2.layers.getContainerOffset(),
-					isVisible: window.getComputedStyle(c2.layers.getContainer()).display !== 'none',
 				},
 			}
 		})
@@ -40,6 +30,31 @@ test.describe('First comment overlay', () => {
 		await page.waitForTimeout(1000)
 
 		const overlayMenu = page.locator('.cd-comment-overlay-menu').first()
+		await expect(overlayMenu).toBeVisible()
+	})
+
+	test('Overlay should appear when hovering the second comment', async ({ page }) => {
+		const secondCommentPart = page.locator('.cd-comment-part-first').nth(1)
+
+		const debugInfo = await page.evaluate(() => {
+			const c2 = window.convenientDiscussions.comments[1]
+			c2.configureLayers()
+
+			return {
+				c2: {
+					container: c2.layers.getContainer().tagName + '.' + c2.layers.getContainer().className,
+					offset: c2.layers.getContainerOffset(),
+					isVisible: window.getComputedStyle(c2.layers.getContainer()).display !== 'none',
+				},
+			}
+		})
+
+		console.log(debugInfo)
+
+		await secondCommentPart.hover()
+		await page.waitForTimeout(1000)
+
+		const overlayMenu = page.locator('.cd-comment-overlay-menu').nth(1)
 		await expect(overlayMenu).toBeVisible()
 	})
 })
