13d99b74c7f0ee7bf48acf18810761e101bb4edb
jwbth <jack.who.built.the.house@outlook.com>
2026-03-25 22:51:16 +0400

    chore: clean up AI artifacts
    Change-Id: I7f1758467009f55dd77db1ca62e7f8c53b09189f

--- debug-jest-config.js
+++
@@ -1,3 +0,0 @@
-import config from './jest.config.js'
-console.log('Jest config loaded successfully')
-console.log('Test Environment:', config.testEnvironment)
--- debug-playwright-config.js
+++
@@ -1,3 +0,0 @@
-import config from './playwright.config.js'
-console.log('Playwright config loaded successfully')
-console.log('Test Dir:', config.testDir)
