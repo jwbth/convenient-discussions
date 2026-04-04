2911e84a478fe08d0bc5cd49988e64a47d385a95
jwbth <jack.who.built.the.house@outlook.com>
2026-04-04 14:34:14 +0400

    revert: "ci(deploy): try hotfix deploying with GitHub Actions"
    Didn't work.
    
    This reverts commit 949f7ab0c12b4012bd0097e202a2b3248ce6b0ce.
    
    Change-Id: Icf9e5fb4fbf0cf8c2ae899c8a74699626e37a259

--- deploy.mjs
+++ deploy.mjs
@@ -121,28 +121,22 @@ const configAssets = config.configs.flatMap((wikiConfig) => {
 
 /** @type {string} */
 let version
-//if (process.env.CI) {
-// HTTP proxy to use with the http-proxy-to-socks module, while the SOCKS proxy is created by the
-// `ssh -D [port]` command as part of the SSH tunnel to Toolforge.
-config.proxy = 'http://localhost:8080'
+if (process.env.CI) {
+	// HTTP proxy to use with the http-proxy-to-socks module, while the SOCKS proxy is created by the
+	// `ssh -D [port]` command as part of the SSH tunnel to Toolforge.
+	config.proxy = 'http://localhost:8080'
 
-// eslint-disable-next-line no-one-time-vars/no-one-time-vars
-const eventJson = JSON.parse(
-	fs.readFileSync(
-		/** @type {string} */ (process.env.GITHUB_EVENT_PATH),
-		'utf8',
-	),
-)
+	// eslint-disable-next-line no-one-time-vars/no-one-time-vars
+	const eventJson = JSON.parse(
+		fs.readFileSync(
+			/** @type {string} */ (process.env.GITHUB_EVENT_PATH),
+			'utf8',
+		),
+	)
 
-// Will be undefined if the event is workflow_dispatch.
-version = eventJson.release?.tag_name
-//}
-
-console.log(
-	process.env.CI
-		? `Running in CI mode. Version: ${version || 'unknown'}`
-		: 'Running in interactive mode.',
-)
+	// Will be undefined if the event is workflow_dispatch.
+	version = eventJson.release?.tag_name
+}
 
 const userAgent =
 	'Convenient Discussions deployer/0.0 (https://commons.wikimedia.org/wiki/User:Jack_who_built_the_house/Convenient_Discussions; User:Jack who built the house)'
