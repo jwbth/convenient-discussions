29e36b3c983cb345e45453ef88b5f62f667e3d02
jwbth <jack.who.built.the.house@outlook.com>
2026-04-06 12:30:44 +0400

    config: add ptwiki config
    Change-Id: Id1922ffe1615e76f14c6449335a6555a9ca7b79f

---
+++ config/wikis/w-pt.js
@@ -0,0 +1,220 @@
+export default /** @type {Partial<typeof import('../default').default>} */ ({
+	messages: {
+		'sun': 'dom',
+		'mon': 'seg',
+		'tue': 'ter',
+		'wed': 'qua',
+		'thu': 'qui',
+		'fri': 'sex',
+		'sat': 'sáb',
+		'sunday': 'domingo',
+		'monday': 'segunda-feira',
+		'tuesday': 'terça-feira',
+		'wednesday': 'quarta-feira',
+		'thursday': 'quinta-feira',
+		'friday': 'sexta-feira',
+		'saturday': 'sábado',
+		'jan': 'jan.',
+		'feb': 'fev.',
+		'mar': 'mar.',
+		'apr': 'abr.',
+		'may': 'mai.',
+		'jun': 'jun.',
+		'jul': 'jul.',
+		'aug': 'ago.',
+		'sep': 'set.',
+		'oct': 'out.',
+		'nov': 'nov.',
+		'dec': 'dez.',
+		'january': 'janeiro',
+		'february': 'fevereiro',
+		'march': 'março',
+		'april': 'abril',
+		'may_long': 'maio',
+		'june': 'junho',
+		'july': 'julho',
+		'august': 'agosto',
+		'september': 'setembro',
+		'october': 'outubro',
+		'november': 'novembro',
+		'december': 'dezembro',
+		'january-gen': 'janeiro',
+		'february-gen': 'fevereiro',
+		'march-gen': 'março',
+		'april-gen': 'abril',
+		'may-gen': 'maio',
+		'june-gen': 'junho',
+		'july-gen': 'julho',
+		'august-gen': 'agosto',
+		'september-gen': 'setembro',
+		'october-gen': 'outubro',
+		'november-gen': 'novembro',
+		'december-gen': 'dezembro',
+		'parentheses': '($1)',
+		'parentheses-start': '(',
+		'parentheses-end': ')',
+		'word-separator': ' ',
+		'comma-separator': ', ',
+		'colon-separator': ': ',
+		'nextdiff': 'Ver a alteração posterior →',
+		'timezone-utc': 'UTC',
+		'pagetitle': '$1 – Wikipédia, a enciclopédia livre',
+		'discussiontools-topicsubscription-button-subscribe': 'Vigiar secção',
+		'discussiontools-topicsubscription-button-subscribe-tooltip':
+			'Subscrever para receber notificações de comentários novos.',
+		'discussiontools-topicsubscription-button-unsubscribe': 'Deixar de vigiar secção',
+		'discussiontools-topicsubscription-button-unsubscribe-tooltip':
+			'Cancelar a subscrição para parar de receber notificações sobre comentários novos.',
+		'discussiontools-topicsubscription-notify-subscribed-title': 'Tópico subscrito!',
+		'discussiontools-topicsubscription-notify-subscribed-body':
+			'Receberá notificações sobre comentários novos neste tópico.',
+		'discussiontools-topicsubscription-notify-unsubscribed-title':
+			'Subscrição do tópico cancelada.',
+		'discussiontools-topicsubscription-notify-unsubscribed-body':
+			'Deixará de receber notificações sobre comentários novos neste tópico.',
+		'discussiontools-newtopicssubscription-button-subscribe-label': 'Subscrever',
+		'discussiontools-newtopicssubscription-button-subscribe-tooltip':
+			'Subscrever para receber notificações quando forem iniciados tópicos novos nesta página.',
+		'discussiontools-newtopicssubscription-button-unsubscribe-label': 'Anular subscrição',
+		'discussiontools-newtopicssubscription-button-unsubscribe-tooltip':
+			'Anular a subscrição para parar de receber notificações quando forem iniciados tópicos novos nesta página.',
+		'discussiontools-newtopicssubscription-notify-subscribed-title': 'Tópico subscrito!',
+		'discussiontools-newtopicssubscription-notify-subscribed-body':
+			'Irá receber notificações quando forem iniciados tópicos novos nesta página.',
+		'discussiontools-newtopicssubscription-notify-unsubscribed-title':
+			'Subscrição do tópico anulada.',
+		'discussiontools-newtopicssubscription-notify-unsubscribed-body':
+			'Deixará de receber notificações quando forem iniciados tópicos novos nesta página.',
+		'thanks-confirmation2': '{{GENDER:$1|Enviar}} um agradecimento que será público?',
+		'checkuser-userinfocard-toggle-button-aria-label':
+			'Abrir ficha de informação do {{GENDER:$1|utilizador}}',
+	},
+	specialPageAliases: {
+		Contributions: ['Contribuições', 'Contribuições', 'Contributions'],
+		Diff: [
+			'Diferenças_entre_edições',
+			'Mudanças_entre_edições',
+			'Mudanças_entre_edições',
+			'Diferenças_entre_edições',
+			'Diff',
+		],
+		PermanentLink: [
+			'Ligação_permanente',
+			'Link_permanente',
+			'Ligação_permanente',
+			'Link_permanente',
+			'PermanentLink',
+		],
+	},
+	thumbAliases: ['miniaturadaimagem', 'miniatura'],
+	timezone: 'UTC',
+	pageWhitelist: [],
+	pageBlacklist: [],
+	userNamespacesByGender: null,
+	genderNeutralUserNamespaceAlias: null,
+	archivePaths: [],
+	pagesWithoutArchives: [],
+	defaultIndentationChar: ':',
+	spaceAfterIndentationChars: true,
+	indentationCharMode: 'mimic',
+	signatureEndingRegexp: / \(discussão\)/,
+	unsignedTemplates: [
+		'Não assinou',
+		'Não assinou2',
+		'Nãoassinou',
+		'Não assinado',
+		'Nao ass',
+		'Nao assinou',
+		'Na',
+		'IP não assinou',
+		'Não assinou IP',
+		'Unsigned IP',
+	],
+	smallDivTemplates: ['smalldiv'],
+	paragraphTemplates: ['pb', 'Quebra de parágrafo'],
+	outdentTemplates: [
+		'outdent',
+		'Retornar indentação',
+		'Ri',
+		'Desindentar',
+		'Recuo',
+		'Od',
+		'Out',
+		'Desindent',
+		'Desind',
+		'Desin',
+	],
+	clearTemplates: ['Limpar', '-', 'Clear', 'Clr'],
+	reflistTalkTemplates: [
+		'Referências-discussão',
+		'Reflist-talk',
+		'Refdis',
+		'Notas-discussão',
+		'Notelist-talk',
+	],
+	noSignatureClasses: [],
+	noSignatureTemplates: [
+		'Movido de',
+		'Copiado de',
+		'Transcrito de',
+		'Trazido de',
+		'Movido para',
+		'Copiado para',
+	],
+	commentAntipatterns: [],
+	excludeFromHeadlineClasses: [],
+	closedDiscussionTemplates: [
+		['Discussão topo', 'Arquivo topo'],
+		['Arquivo rodapé', 'MP baixo'],
+	],
+	closedDiscussionClasses: [],
+});
+
+/** @type {CSSStyleSheet} */
+let styles;
+mw.hook('convenientDiscussions.beforeParse').add(function () {
+	if (!styles) {
+		styles = mw.util.addCSS(
+			'\
+.sitedir-ltr #mw-content-text .cd-commentLevel:not(ol) > dd,\
+.sitedir-ltr #mw-content-text .cd-commentLevel:not(ol) > li,\
+.sitedir-rtl #mw-content-text .mw-content-ltr .cd-commentLevel:not(ol) > dd,\
+.sitedir-rtl #mw-content-text .mw-content-ltr .cd-commentLevel:not(ol) > li {\
+	padding-left: 1em;\
+	padding-right: 0;\
+	margin-left: 1em;\
+	margin-right: 0;\
+}\
+\
+.skin-vector #mw-content-text .cd-parsed dd,\
+.skin-vector #mw-content-text .cd-parsed li,\
+.skin-vector #mw-content-text .cd-parsed ol > li.cd-comment-part-last.cd-comment-part-last {\
+	margin-bottom: 0.14285714em;\
+}\
+\
+.cd-reformattedComments #mw-content-text .cd-comment-part-first {\
+	margin-top: 0.75em;\
+}\
+\
+#mw-content-text .cd-comment-part-first {\
+	margin-top: 0.5em;\
+}\
+\
+.ns-talk #mw-content-text#mw-content-text dl {\
+	border-top: 0;\
+	border-left: 0;\
+	padding-top: 0;\
+	padding-left: 0;\
+	margin-left: 0;\
+	background: none;\
+}\
+\
+#mw-content-text#mw-content-text .cd-replyButtonWrapper {\
+	margin-top: 0.5em;\
+}\
+',
+		);
+	}
+});
+
+// </nowiki>
