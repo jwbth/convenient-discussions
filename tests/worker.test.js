3acba1a241a8473959426d7799c9321e05a96700
jwbth <jack.who.built.the.house@outlook.com>
2026-04-10 23:03:19 +0400

    test(worker): fix basic tests
    Change-Id: I92a472849588d228adbe0eea2f6de58a3a4b3dc3

--- tests/worker.test.js
+++ tests/worker.test.js
@@ -24,8 +24,7 @@ describe('worker', () => {
 	})
 
 	test('parse identifies comment, author, and date correctly', () => {
-		const text =
-			'<div class="mw-content-ltr mw-parser-output" lang="en" dir="ltr"><p>Comment. <a href="/wiki/User:Example" title="User:Example">Example</a> (<a href="/wiki/User_talk:Example" title="User talk:Example">talk</a>) 00:00, 1 January 2026 (UTC)\n</p></div>'
+		const text = `<div class="mw-content-ltr mw-parser-output" lang="en" dir="ltr"><div class="mw-heading mw-heading2"><h2 id="Section">Section</h2></div>\n<p>Comment. <a href="/wiki/User:Example" title="User:Example">Example</a> (<a href="/wiki/User_talk:Example" title="User talk:Example">talk</a>) 00:00, 1 January 2026 (UTC)\n</p></div>`
 
 		const payload = structuredClone(defaultPayload)
 		payload.text = text
@@ -38,13 +37,15 @@ describe('worker', () => {
 		expect(msg).toBeDefined()
 		const parseResult = msg[0]
 
+		console.log(parseResult.comments[0])
+
 		expect(parseResult.comments).toHaveLength(1)
 		expect(parseResult.comments[0].authorName).toBe('Example')
-		expect(parseResult.comments[0].date).toBeDefined()
+		expect(parseResult.comments[0].date.toISOString()).toBe('2026-01-01T00:00:00.000Z')
 	})
 
 	test('parse identifies sections correctly', () => {
-		const text = `<div class="mw-content-ltr mw-parser-output" lang="en" dir="ltr"><h2 class="mw-heading"><span class="mw-headline" id="Test_Section">Test Section</span></h2><p>Another comment. <a href="/wiki/User:Example2" title="User:Example2">Example2</a> (<a href="/wiki/User_talk:Example2" title="User talk:Example2">talk</a>) 01:00, 1 January 2026 (UTC)\n</p></div>`
+		const text = `<div class="mw-content-ltr mw-parser-output" lang="en" dir="ltr"><div class="mw-heading mw-heading2"><h2 id="Section">Section</h2></div>\n<p>Comment. <a href="/wiki/User:Example" title="User:Example">Example</a> (<a href="/wiki/User_talk:Example" title="User talk:Example">talk</a>) 00:00, 1 January 2026 (UTC)\n</p></div>>`
 
 		const payload = structuredClone(defaultPayload)
 		payload.text = text
@@ -55,8 +56,8 @@ describe('worker', () => {
 		const parseResult = msg[0]
 
 		expect(parseResult.sections).toHaveLength(1)
-		expect(parseResult.sections[0].headline).toBe('Test Section')
+		expect(parseResult.sections[0].headline).toBe('Section')
 		expect(parseResult.comments).toHaveLength(1)
-		expect(parseResult.comments[0].authorName).toBe('Example2')
+		expect(parseResult.comments[0].authorName).toBe('Example')
 	})
 })
