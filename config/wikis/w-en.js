511c3c5ca391fb8a7cff86f67d2f601df8dcac45
jwbth <jack.who.built.the.house@outlook.com>
2026-06-10 12:44:40 +0400

    config(w-en): remove boilerplate class from closed discussion classes
    Kills reply buttons at https://en.wikipedia.org/wiki/Project:Contributor_copyright_investigations.
    
    Change-Id: I57de1ba952d15fc27ce8d04044b2c8646b9b4e43

--- config/wikis/w-en.js
+++ config/wikis/w-en.js
@@ -561,7 +561,7 @@ export default /** @type {Partial<typeof import('../default').default>} */ ({
 		],
 	],
 
-	closedDiscussionClasses: ['archived', 'boilerplate', 'hat-ai-content'],
+	closedDiscussionClasses: ['archived', 'hat-ai-content'],
 
 	noHighlightClasses: ['infobox', 'unresolved', 'resolved', 'hmbox-outer'],
 
