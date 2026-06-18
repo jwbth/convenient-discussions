jwbth/convenient-discussions

commit 799f91922e0dea16db4dd3f5a25f3a347477c168

fix(Section): don't allow to move section if user can't edit page
Change-Id: I48b77a932abbd02f06cee0f33ed74785fa215a3d

src/Section.js
@@ -630,7 +630,12 @@ class Section extends SectionSkeleton {
 	 * @returns {boolean}
 	 */
 	canBeMoved() {
-		return this.isTopic() && !this.isTranscludedFromTemplate() && cd.page.isActive()
+		return (
+			mw.config.get('wgIsProbablyEditable') &&
+			this.isTopic() &&
+			!this.isTranscludedFromTemplate() &&
+			cd.page.isActive()
+		)
 	}
 
 	/**
@@ -639,12 +644,7 @@ class Section extends SectionSkeleton {
 	 * @returns {boolean}
 	 */
 	canBeUnarchived() {
-		return (
-			this.isTopic() &&
-			!this.isTranscludedFromTemplate() &&
-			cd.page.isArchive() &&
-			cd.page.getArchivedPage() !== cd.page
-		)
+		return this.canBeMoved() && cd.page.getArchivedPage() !== cd.page
 	}
 
 	/**
@@ -915,7 +915,7 @@ class Section extends SectionSkeleton {
 	}
 
 	/**
-	 * Is the section actionable.
+	 * Is the section actionable. The section can still be moved if it's not actionable.
 	 *
 	 * @returns {boolean}
 	 */

commit 427d801f6ac7ebff3899031d06818084af48199e

fix(Section): canBeUnarchived() shouldn't be based on canBeMoved()
regression in 799f91922e0dea16db4dd3f5a25f3a347477c168

Change-Id: I1bc1eae8eb344f8b7e6a1412f73383781f81e7ba

src/Section.js
@@ -644,7 +644,12 @@ class Section extends SectionSkeleton {
 	 * @returns {boolean}
 	 */
 	canBeUnarchived() {
-		return this.canBeMoved() && cd.page.getArchivedPage() !== cd.page
+		return (
+			this.isTopic() &&
+			!this.isTranscludedFromTemplate() &&
+			cd.page.isArchive() &&
+			cd.page.getArchivedPage() !== cd.page
+		)
 	}
 
 	/**